65cfeffa149f5eb5ed218822453926cd8e7cb64d
Daniel Rosel <daniel@alves.world>
2024-10-04 17:06:40 +0200

    added actual code for home page

---
+++ src/components/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="home">
+    <h1>Welcome to the IE Bank Page</h1>
+    <button @click="goToAccounts">View Accounts</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+  methods: {
+    goToAccounts() {
+      this.$router.push('/accounts')
+    }
+  }
+}
+</script>
+
+<style scoped>
+.home {
+  text-align: center;
+  padding: 20px;
+}
+
+button {
+  padding: 10px 20px;
+  font-size: 16px;
+  cursor: pointer;
+}
+</style>
