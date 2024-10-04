6ef9e876d5a84777dd499f09db33f454aae0f9ae
Daniel Rosel <daniel@alves.world>
2024-10-04 17:06:32 +0200

    added route for home page

--- src/router/index.js
+++ src/router/index.js
@@ -2,20 +2,26 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Skull from '../components/Skull.vue'
 import AppAccounts from '../components/AppAccounts.vue'
+import Home from '../components/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/skull',
-    name: 'Skull',
-    component: Skull
-  },
-  {
-    path: '/accounts',
-    name: 'AppAccounts',
-    component: AppAccounts
-  }
+    {
+        path: '/skull',
+        name: 'Skull',
+        component: Skull
+    },
+    {
+        path: '/accounts',
+        name: 'AppAccounts',
+        component: AppAccounts
+    },
+    {
+        path: '/',
+        name: 'Home',
+        component: Home
+    }
 ]
 
 const router = new VueRouter({
